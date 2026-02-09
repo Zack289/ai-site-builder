a97329f2fd01ff303cce6e2868b71a17d995220a
Zack289 <ozack911@gmail.com>
2026-02-09 23:47:49 +0545

    Added the footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return (
+    <div className="text-center py-4 text-gray-400 text-sm-border border-gray-800 mt-24">
+      <p>Copyright @ 2026 AI Website Builder.</p>
+    </div>
+  )
+}
+
+export default Footer
