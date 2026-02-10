264625438d991106203d9fe13df3fdb9d70479c4
Zack289 <ozack911@gmail.com>
2026-02-10 17:55:56 +0545

    Added the builder/project section to the project

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,10 +1,178 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  ArrowBigDownDashIcon,
+  EyeIcon,
+  EyeOffIcon,
+  FullscreenIcon,
+  LaptopIcon,
+  Loader2Icon,
+  MessageSquareIcon,
+  SaveIcon,
+  SmartphoneIcon,
+  TabletIcon,
+  XIcon,
+} from "lucide-react";
+import type { Project } from "../types";
+import { dummyConversations, dummyProjects } from "../assets/assets";
 
 function Projects() {
-  return (
-    <div>
-      Projects
+  const [project, setProject] = useState<Project | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const [isGenerating, setIsGenerating] = useState(true);
+  const [device, setDevice] = useState<"phone" | "tablet" | "desktop">(
+    "desktop",
+  );
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
+
+  const { projectId } = useParams();
+  const navigate = useNavigate();
+
+  const fetchProject = async () => {
+    const project = dummyProjects.find((project) => project.id === projectId);
+
+    setTimeout(() => {
+      if (project) {
+        setProject({ ...project, conversation: dummyConversations });
+        setLoading(false);
+        setIsGenerating(project.current_code ? false : true);
+      }
+    }, 2000);
+  };
+
+  const saveProject = async () => {};
+
+  const downloadCode = () => {};
+
+  const togglePublish = async () => {};
+
+  useEffect(() => {
+    fetchProject();
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <div className="flex items-center justify-center h-screen">
+          <Loader2Icon className="size-7 animate-spin text-violet-200" />
+        </div>
+      </>
+    );
+  }
+  return project ? (
+    <div className="flex flex-col h-screen w-full bg-gray-900 text-white">
+      {/* builder navbar */}
+      <div className="flex max-sm:flex-col sm:items-center gap-4 px-4 py-2 no-scrollbar">
+        {/* left  */}
+        <div className="flex items-center gap-2 sm:min-w-90 text-nowrap">
+          <img
+            src="/favicon.svg"
+            alt="logo"
+            className="h-6 cursor-pointer"
+            onClick={() => navigate("/")}
+          />
+        </div>
+
+        <div className="max-w-64  sm:max-w-xs">
+          <p className="text-sm text-medium capitalize truncate">
+            {project.name}
+          </p>
+          <p className="text-xs text-gray-400 -mt-0.5">
+            Preview last saved saved version
+          </p>
+        </div>
+
+        <div className="sm:hidden flex-1 fleex justify-end">
+          {isMenuOpen ? (
+            <MessageSquareIcon
+              onClick={() => setIsMenuOpen(false)}
+              className="size-6 cursor-pointer"
+            />
+          ) : (
+            <XIcon
+              onClick={() => setIsMenuOpen(true)}
+              className="size-6 cursor-pointer"
+            />
+          )}
+        </div>
+
+        {/* middle  */}
+        <div className="hidden sm:flex gap-2 bg-gray-950 p-1.5 rounded-md">
+          <SmartphoneIcon
+            onClick={() => setDevice("phone")}
+            className={`size-6 p-1 rounded cursor-pointer ${device === "phone" ? "bg-gray-700" : ""}`}
+          />
+
+          <TabletIcon
+            onClick={() => setDevice("tablet")}
+            className={`size-6 p-1 rounded cursor-pointer ${device === "tablet" ? "bg-gray-700" : ""}`}
+          />
+
+          <LaptopIcon
+            onClick={() => setDevice("desktop")}
+            className={`size-6 p-1 rounded cursor-pointer ${device === "desktop" ? "bg-gray-700" : ""}`}
+          />
+        </div>
+
+        {/* right  */}
+        <div className="flex items-center justify-end gap-3 flex-1 text-xs sm:text-sm">
+          <button
+            onClick={saveProject}
+            disabled={isSaving}
+            className="max-sm:hidden bg-gray-800 hover:bg-gray-700 text-white px-3.5 py-1 flex items-center gap-2 rounded sm:rounded-sm transition-colors border border-gray-700 "
+          >
+            {isSaving ? (
+              <Loader2Icon className="animate-spin" size={16} />
+            ) : (
+              <SaveIcon size={16} />
+            )}
+            Save
+          </button>
+
+          <Link
+            to={`/preview/${project.id}`}
+            target="_blank"
+            className="flex items-center gap-2 px-4 py-1  rounded sm:rounded-sm border border-gray-700 hover:border-gray-500 transition-colors"
+          >
+            <FullscreenIcon size={16} /> Preview
+          </Link>
+
+          <button
+            onClick={downloadCode}
+            className="bg-linear-to-br from-blue-700 to-blue-700 hover:from-blue-600 hover:to-blue-500 text-white px-3.5 py-1 flex items-center gap-2 rounded sm:rounded-sm transition-colors"
+          >
+            <ArrowBigDownDashIcon size={16} />
+            Download
+          </button>
+
+          <button
+            onClick={togglePublish}
+            className="bg-linear-to-br from-blue-700 to-blue-700 hover:from-blue-600 hover:to-blue-500 text-white px-3.5 py-1 flex items-center gap-2 rounded sm:rounded-sm transition-colors"
+          >
+            {project.isPublished ? (
+              <EyeOffIcon size={16} />
+            ) : (
+              <EyeIcon size={16} />
+            )}
+            {project.isPublished ? "Unpublish" : "Publish"}
+          </button>
+        </div>
+      </div>
+
+      <div>
+        
+      </div>
     </div>
-  )
+  ) : (
+    <div className="flex items-center justify-center h-screen">
+      <p className="text-2xl font-medium text-gray-200">
+        Unable to load Projects!😵‍💫
+      </p>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
